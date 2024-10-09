786af23d70ef15569cd236bf13a2a2eb5e8baf6e
Jordan26-dev <jordan26.online@gmail.com>
2024-10-09 16:08:36 +0800

    generate keys

--- generate_key.py
+++ generate_key.py
@@ -3,15 +3,20 @@ from pathlib import Path
 
 import streamlit as st
 import streamlit_authenticator as stauth
-from streamlit_authenticator.utilities.hasher import Hasher
+
 
 names = ["Peter Parker", "Rebecca Miller"]
 usernames = ["pparker", "rmiller"]
-passwords = ["abc123", "def456"]
+passwords = ["XXX", "XXX"]
 
-hashed_passwords = Hasher(passwords).generate()
+# Create an instance of the Hasher class
+hasher = stauth.Hasher(passwords)
+
+# Generate hashed passwords
+hashed_passwords = [hasher.hash(password) for password in passwords]
 
 print(hashed_passwords)
+
 file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open("wb") as file:
     pickle.dump(hashed_passwords, file)
